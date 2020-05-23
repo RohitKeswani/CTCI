2687b672e6bb8e97574db4a11b06d05a1a6e5ffc
RohitKeswani <rhareshk@gmail.com>
2020-05-22 21:23:44 -0400

    ordered linked list

--- src/LinkedLists/Question5.java
+++ src/LinkedLists/Question5.java
@@ -1,10 +1,46 @@
 package LinkedLists;
 
+import java.util.Stack;
+
 public class Question5 {
     public static void main(String[] args) {
-        ListNode firstList = ListNode.createList(new int[]{8,7,6});
+        ListNode firstList = ListNode.createList(new int[]{8,7,6}); // number = 678
         ListNode secondList = ListNode.createList(new int[]{9,4});
         addList(firstList, secondList);
+        firstList = ListNode.createList(new int[]{6,1,7}); //number = 876
+        secondList = ListNode.createList(new int[]{2,9,5});
+        addListOrdered(firstList, secondList);
+    }
+
+    private static void addListOrdered(ListNode firstList, ListNode secondList) {
+        Stack<Integer> firstStack = new Stack<>();
+        while(firstList!=null){
+            firstStack.push(firstList.val);
+            firstList=firstList.next;
+        }
+        Stack<Integer> secondStack = new Stack<>();
+        while(secondList!=null){
+            secondStack.push(secondList.val);
+            secondList=secondList.next;
+        }
+        int sum, carry = 0;
+        Stack<Integer> resultStack = new Stack<>();
+        while(!(firstStack.isEmpty()) || !(secondStack.isEmpty())){
+            sum = carry + (firstStack.isEmpty()?0:firstStack.pop()) + (secondStack.isEmpty()?0:secondStack.pop());
+            if(sum > 9){
+                carry = 1;
+                sum = sum % 10;
+            }
+            else
+                carry = 0;
+            resultStack.push(sum);
+        }
+        if(carry == 1){
+            resultStack.push(carry);
+        }
+        while(!resultStack.isEmpty()){
+            System.out.print(resultStack.pop()+"");
+        }
     }
 
     private static void addList(ListNode firstList, ListNode secondList) {
