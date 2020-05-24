ef5a3b8f36568b602933b5301f9e5e13ce2d1ba2
RohitKeswani <rhareshk@gmail.com>
2020-05-24 17:07:52 -0400

    LL:Q6 using stack not using length

--- src/LinkedLists/Question6.java
+++ src/LinkedLists/Question6.java
@@ -5,38 +5,25 @@ import java.util.Stack;
 public class Question6 {
     public static void main(String[] args) {
         ListNode head = ListNode.createList(new int[]{1,2,5,2,1});
-        boolean isPallindrome = checkIsPallindrome(head);
-        System.out.println(isPallindrome);
+        boolean isPallindrome = checkIsPallindromeWithStack(head);
+        System.out.println("Stack: "+isPallindrome);
     }
 
-    private static boolean checkIsPallindrome(ListNode head) {
-        int length = findLength(head);
+    private static boolean checkIsPallindromeWithStack(ListNode head) {
         Stack<Integer> pallindromCheck = new Stack<>();
-        int count = 0;
-        while(count<length/2){
-            pallindromCheck.push(head.val);
-            head = head.next;
-            count++;
+        ListNode temp = head;
+        while(temp!=null){
+            pallindromCheck.push(temp.val);
+            temp = temp.next;
         }
-        if(length%2==1)
-            head = head.next;
-        while(!pallindromCheck.isEmpty() && head!=null){
-            if(head.val==pallindromCheck.pop()){
-                head = head.next;
+        temp = head;
+        while(temp!=null && !pallindromCheck.isEmpty()){
+            if(temp.val == pallindromCheck.pop()){
+                temp = temp.next;
             }
             else
                 return false;
         }
-        return pallindromCheck.isEmpty() && head == null;
-    }
-
-    private static int findLength(ListNode head) {
-        int len = 0;
-        ListNode temp = head;
-        while(temp!=null){
-            len++;
-            temp = temp.next;
-        }
-        return len;
+        return pallindromCheck.isEmpty() && temp == null;
     }
 }
